159bce8448564d373a3761cba82206be4f735bac
Luka Trempetić <trempetic92@gmail.com>
2024-05-09 20:15:04 +0200

    push update

--- V01_Footbal_league_test/LeagueControllerTest.cs
+++ V01_Footbal_league_test/LeagueControllerTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using V01_Footbal_league.Controllers;
 using V01_Footbal_league.Models;
 using V01_Footbal_league_test.Mock;
+using Moq;
 
 namespace V01_Footbal_league_test
 {
@@ -30,6 +31,30 @@ namespace V01_Footbal_league_test
             var result = controller.Index();
 
             Assert.IsAssignableFrom<ViewResult>(result);
+            mockLeagueRepository.VerifyGetAll(Times.Once());
         }
+
+        [Fact]
+        public void LeagueController_Index_noLeagues()
+        {
+            var mockLeagues = new List<League>()
+            {
+                new League()
+                {
+                    Id = 1,
+                    DateEstablished = new DateTime(2000,3,25)
+                }
+            };
+
+            var mockLeagueRepository = new MockLeagueRepository().MockGetAll(new List<League>);
+            var controller = new LeagueController(mockLeagueRepository.Object);
+
+            var result = controller.Index();
+
+            Assert.IsAssignableFrom<ViewResult>(result);
+            mockLeagueRepository.VerifyGetAll(Times.Once());
+        }
+
+
     }
 }
