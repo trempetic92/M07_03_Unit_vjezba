575358248ce99e6fc61101c3f9161a14e306ed3a
Luka Trempetić <trempetic92@gmail.com>
2024-05-09 20:36:10 +0200

    push

--- V01_Footbal_league_test/TeamControllerTest.cs
+++ V01_Footbal_league_test/TeamControllerTest.cs
@@ -47,5 +47,28 @@ namespace V01_Footbal_league_test
             Assert.IsAssignableFrom<ViewResult>(result);
             mockTeamRepository.VerifySearch(Times.Once());
         }
+
+        [Fact]
+        public void TeamController_Search_Post_ModelStateInvalid()
+        {
+            var mockTeams = new List<Team>()
+            {
+                new Team
+                {
+                    Id = 1
+                }
+            };
+            var mockTeamRepository = new MockTeamRepository().MockSearch(mockTeams);
+            var controller = new TeamControler(mockTeamRepository.Object);
+            controller.ModelState.AddModelError("Error", "Test");
+
+            var result = controller.Search(new TeamSearch());
+
+            Assert.IsAssignableFrom<ViewResult>(result);
+            mockTeamRepository.VerifySearch(Times.Once());
+
+            var redirectAtion = (RedirectToActionResult)result;
+            Assert.Equal("Search", redirectAtion.ActionName);
+        }
     }
 }
